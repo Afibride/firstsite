9dd9039d7b27528ec8252f0ad370d1c0ce9df800
Afibride <afibright07@gmail.com>
2024-09-12 14:56:50 +0100

    updates

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,6 +15,7 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
     public function definition()
     {
         return [
@@ -22,6 +23,11 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'phone' => fake()->phoneNumber,
+            'address' => fake()->address,
+            'photo' => fake()->imageUrl('60','60'),
+            'role' => fake()->randomElement(['Admin','agent','user']),
+            'status' => fake()->randomElement(['Active','Inactive']),
             'remember_token' => Str::random(10),
         ];
     }
